c21d2fe45334048ac22202b7ad11fc04572f549e
meganclapinski25 <142531799+meganclapinski25@users.noreply.github.com>
2024-01-11 12:12:43 -0800

    starting with database configuration

--- app.py
+++ app.py
@@ -1,29 +1,65 @@
 import os
-import requests
-import openai
-
-from dotenv import load_dotenv
-from flask import Flask, request, redirect, render_template, url_for
+from flask import Flask, request, render_template, redirect, url_for, session
 from flask_pymongo import PyMongo
+from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+import certifi
+import openai
+import bcrypt
+
+
+
+
+# get this path from the panel on mongodb.com
+#meganclapinski
+#pjqBwnbY83tONHQ6
 
 
 
 
 app = Flask(__name__)
+load_dotenv()
+
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+mongo = PyMongo(app)
+
 
 openai.api_key = os.environ.get('API_KEY')
 
-load_dotenv
+
+
+
 API_KEY = os.getenv('API_KEY')
 API_URL = 'https://api.openai.com/v1/chat/completions'
 
 @app.route('/')
 def homepage():
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
     return render_template('index.html')
 
 @app.route('/workoutpage')
 def workoutpage():
     return render_template('workoutex.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        returning_user = mongo.db.users.find_one({'username': username})
+        if returning_user:
+            return render_template('index.html')
+        
+        user_data = {'username': username, 'password': password}
+        mongo.db.users.insert_one(user_data)
+        session['user'] = username
+        
+        return redirect(url_for('homepage'))
+    
+    return render_template('index.html')
+
 
 @app.route('/information', methods=['POST'])
 def process_information():
@@ -34,10 +70,6 @@ def process_information():
     calories = request.form.get('calorie')
 
     return redirect(url_for('workoutgen', height=height, weight=weight, program=program, calorie=calories))
-
-
-
-
 
 @app.route('/chatgbt_workout', methods=['GET'])
 def workoutgen():
